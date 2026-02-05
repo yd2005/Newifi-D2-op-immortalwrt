f7511f2eacc3af4733368e82b0d01611a0c6791b
yd2005 <76555598+yd2005@users.noreply.github.com>
2026-02-05 16:22:51 +0800

    Update diy-part1-newifi3.sh

--- scripts/diy-part1-newifi3.sh
+++ scripts/diy-part1-newifi3.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # diy-part1-newifi3.sh
 
-# 添加 Nikki 源
-echo 'src-git nikki https://github.com/nikki-dev/luci-app-nikki' >> feeds.conf.default
-
-# 可以在这里添加其他源，例如 Passwall 或 SSR+，但基于你的需求 Nikki 已足够
+# 这里什么都不用加了，Nikki 我们在 Part 2 用 git clone 安装
+# 保持为空或仅注释即可
